faea71b2ac191969a6f371daf84376d55fefdc92
Aditya Dhiman <info@adityadhiman.in>
2024-09-12 20:22:44 +0530

    setup frontend routes

---
+++ client/src/state/index.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    mode: "light",
+    user: null,
+    token : null,
+    posts : []
+}
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers:{
+        setMode : (state)=>{
+            state.mode = state.mode === "light"? "dark" : "light";
+        },
+        setLogin: (state, action)=>{
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+        setLogout : (state)=>{
+            state.user = null;
+            state.token = null;
+        },
+        setFriends : (state, action)=>{
+            if(state.user){
+                state.user.friends = action.payload.friends;
+            }
+            else{
+                console.log("user friends not existed");
+            }
+        }
+    }
+})

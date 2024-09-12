f04d703eacafbd638be91bd6c2d06534dbccedaa
Aditya Dhiman <info@adityadhiman.in>
2024-09-12 20:13:19 +0530

    frontend updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
-import React from "react";
-import Navbar from "./Components/Navbar/Navbar";
-import Home from "./Components/Home/Home";
+import React from 'react';
+import { BrowserRouter, Navigate, Routes, Route, Router } from 'react-router-dom';
+import HomePage from './scenes/homePage/HomePage.jsx';
+import LoginPage from './scenes/loginPage/LoginPage.jsx';
+import ProfilePage from './scenes/profilePage/profilePage.jsx';
 
-function App(){
-  return{
-    render(){
-      return(
-        <div>
-          <Navbar />
-          <Home />
-        </div>
-      );
-    }
-  }
+
+function App() {
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <Router>
+          <Route path='/' element={LoginPage} />
+          <Route path='/home' element={HomePage} />
+          <Route path='/profile/:userId' element={ProfilePage} />
+
+        </Router>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App

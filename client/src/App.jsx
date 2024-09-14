b20a4140707fa4b46109f30667c8ded8c6efad1d
Aditya Dhiman <info@adityadhiman.in>
2024-09-14 13:13:38 +0530

    frontend completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Navigate, Routes, Route, Router } from 'react-router-dom';
+import React, { useMemo } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './scenes/homePage/HomePage.jsx';
 import LoginPage from './scenes/loginPage/LoginPage.jsx';
-import ProfilePage from './scenes/profilePage/profilePage.jsx';
-
+import ProfilePage from './scenes/profilePage/ProfilePage.jsx';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme.js';
+import Navbar from './scenes/navbar/navbar.jsx';
 
 function App() {
-  return (
-    <div className='app'>
-      <BrowserRouter>
-        <Router>
-          <Route path='/' element={LoginPage} />
-          <Route path='/home' element={HomePage} />
-          <Route path='/profile/:userId' element={ProfilePage} />
+  const mode = useSelector((state) => state.mode);
 
-        </Router>
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/nav" element={<Navbar />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -29,6 +29,23 @@ export const authSlice = createSlice({
             else{
                 console.log("user friends not existed");
             }
+        },
+        setPosts : (state, action) =>{
+            state.posts = action.payload.posts;
+        },
+        setPost : (state, action) =>{
+            const updatedPosts = state.posts.map(post =>{
+                if(post._id === action.payload.post._id){
+                    return action.payload.post;
+                }
+                return post;
+            });
+        },
+        deletePost : (state, action) =>{
+            const updatedPosts = state.posts.filter(post => post._id!== action.payload.postId);
+            state.posts = updatedPosts;
         }
     }
-})
+});
+export const {setMode, setLogin,setLogout, setFriends, setPost, setPosts, deletePost} = authSlice.actions;
+export default authSlice.reducer;
